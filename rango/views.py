c412be2b9f89d2db5fbb82d425cfb7e04289c1e4
Sadaf <2843945A@student.gla.ac.uk>
2023-03-20 13:23:48 +0000

    Test for chapter3 completed

--- rango/views.py
+++ rango/views.py
@@ -9,4 +9,4 @@ def index(request):
 
 def about(request):
     return HttpResponse("Rango says here is the about page.<br/>"+
-    " <a href='/rango'>Index</a>")
+    " <a href='/rango/'>Index</a>")
